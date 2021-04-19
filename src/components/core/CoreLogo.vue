8466688d2a0d8c7b3f94db68d85e61dcd7d6d17e
khanr <khanriza@gmail.com>
2021-04-19 18:28:02 -0400

    new logos

---
+++ src/components/core/CoreLogo.vue
@@ -0,0 +1,35 @@
+<template>
+  <img
+    class="logo"
+    :src="require(`@/assets/img/${logo}`)"
+    :style="{ height, width }"
+  />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "CoreLogo",
+  props: {
+    logo: {
+      type: String,
+      default: "logo.png",
+    },
+    height: {
+      type: String,
+      default: "72px",
+    },
+    width: {
+      type: String,
+      default: "72px",
+    },
+  },
+});
+</script>
+
+<style scoped lang="scss">
+.logo {
+  margin: auto 0;
+}
+</style>
