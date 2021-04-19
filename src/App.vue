373bfff0576063a539410e6f307a360769a7cf8c
khanr <khanriza@gmail.com>
2021-04-19 18:27:30 -0400

    proper way to import in ts

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import Header from "@/components/Menu";
+import Header from "@/components/Menu.vue";
 
 export default defineComponent({
   components: { Header },
