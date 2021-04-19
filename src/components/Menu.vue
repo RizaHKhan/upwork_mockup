5cf7350c5ffb3091ac3234cf116541166996aad5
khanr <khanriza@gmail.com>
2021-04-19 17:08:27 -0400

    created menu component

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,5 +1,23 @@
 <template>
-  <div class="header"></div>
+  <div class="header">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
 </template>
 
 <script lang="ts"></script>
+
+<style lang="scss" scoped>
+.header {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
