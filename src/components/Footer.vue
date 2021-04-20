96ed2bf0422ad011a1be15df9d09b31b8603159c
khanr <khanriza@gmail.com>
2021-04-19 20:00:41 -0400

    added and completed footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="footer">
+    <CoreLogo logo="logo-white.svg" />
+    <p>Copyright 2021 Company Name</p>
+    <a>Return to Top</a>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import CoreLogo from "./core/CoreLogo.vue";
+
+export default defineComponent({
+  name: "Footer",
+  components: { CoreLogo },
+});
+</script>
+
+<style lang="scss" scoped>
+.footer {
+  background: #333333;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 58px 10px;
+  font-size: 18px;
+
+  p {
+    margin: 24px 0;
+    font-weight: 200;
+  }
+
+  a {
+    text-decoration: underline;
+
+    &:hover {
+      cursor: pointer;
+    }
+  }
+}
+</style>
---
+++ src/components/core/CoreBreaker.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="breaker"></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "CoreBreaker",
+});
+</script>
+
+<style lang="scss" scoped>
+.breaker {
+  width: 100%;
+  height: 13px;
+  border: 1px solid #3300ff;
+}
+</style>
